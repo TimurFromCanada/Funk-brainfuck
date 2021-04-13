4551d337836127cbd055675ca6ec1687806a6ba3
timurfromcanada <timurfromcanada@gmail.com>
2021-04-13 09:35:02 +0500

    Implemented VirtualMachine.cs

--- VirtualMachine.cs
+++ VirtualMachine.cs
@@ -5,23 +5,35 @@ namespace func.brainfuck
 {
 	public class VirtualMachine : IVirtualMachine
 	{
+		public Dictionary<char, Action<IVirtualMachine>> Dictionary = new Dictionary<char, Action<IVirtualMachine>>();
+
+		public VirtualMachine(string program, int memorySize)
+		{
+			Instructions = program;
+			MemoryPointer = 0;
+			Memory = new byte[memorySize];
+			InstructionPointer = 0;
+		}
+
+		public void RegisterCommand(char symbol, Action<IVirtualMachine> execute)
+		{
+			Dictionary.Add(symbol, execute);
+		}
+
 		public string Instructions { get; }
 		public int InstructionPointer { get; set; }
 		public byte[] Memory { get; }
 		public int MemoryPointer { get; set; }
 
-		public VirtualMachine(string program, int memorySize)
-		{
-		}
-
-		public void RegisterCommand(char symbol, Action<IVirtualMachine> execute)
-		{
-			throw new NotImplementedException();
-		}
-
 		public void Run()
 		{
-			throw new NotImplementedException();
+			for (; InstructionPointer < Instructions.Length; InstructionPointer++)
+			{
+				var command = Instructions[InstructionPointer];
+
+				if (Dictionary.ContainsKey(command))
+					Dictionary[command](this);
+			}
 		}
 	}
 }
