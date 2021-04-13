cd493e675b4d8fcb95bef54b1af69b6c76df5941
timurfromcanada <timurfromcanada@gmail.com>
2021-04-13 09:37:32 +0500

    Implemented BrainfuckLoopCommands.cs

--- BrainfuckLoopCommands.cs
+++ BrainfuckLoopCommands.cs
@@ -2,12 +2,48 @@ using System.Collections.Generic;
 
 namespace func.brainfuck
 {
-	public class BrainfuckLoopCommands
-	{
-		public static void RegisterTo(IVirtualMachine vm)
-		{
-			vm.RegisterCommand('[', b => { });
-			vm.RegisterCommand(']', b => { });
-		}
-	}
+    public class BrainfuckLoopCommands
+    {
+        class Instruction
+        {
+            public readonly Dictionary<int, int> End;
+            public readonly Dictionary<int, int> Begin;
+
+            public Instruction()
+            {
+                End = new Dictionary<int, int>();
+                Begin = new Dictionary<int, int>();
+            }
+
+            public void AddToDictionary(string instructions)
+            {
+                var stack = new Stack<int>();
+
+                for (var i = 0; i < instructions.Length; i++)
+                {
+                    if (instructions[i] == '[')
+                        stack.Push(i);
+
+                    if (instructions[i] == ']')
+                    {
+                        Begin[stack.Peek()] = i;
+                        End[i] = stack.Pop();
+                    }
+                }
+            }
+        }
+
+        public static void RegisterTo(IVirtualMachine vm)
+        {
+            var instruction = new Instruction();
+            instruction.AddToDictionary(vm.Instructions);
+
+            vm.RegisterCommand('[', b =>
+            {
+                if (b.Memory[b.MemoryPointer] == 0)
+                    b.InstructionPointer = instruction.Begin[b.InstructionPointer];
+            });
+            vm.RegisterCommand(']', b => b.InstructionPointer = instruction.End[b.InstructionPointer] - 1);
+        }
+    }
 }
